73140874797888b634d9cd071a5bcf29c0d03a77
Hao <haojiang@kth.se>
2016-08-01 18:45:03 +0200

    add track4

--- software/openvisualizer/openvisualizer/openLbr/openLbr.py
+++ software/openvisualizer/openvisualizer/openLbr/openLbr.py
@@ -137,7 +137,7 @@ class OpenLbr(eventBusClient.eventBusClient):
         self.sendWithBier         = False
         self.bierAuto             = False
         self.bierLock             = threading.Lock()
-        self.trackID              = 3
+        self.trackID              = 4
         self.bierBitmap           = '111111111111'
 
         # initialize parent class
@@ -283,11 +283,10 @@ class OpenLbr(eventBusClient.eventBusClient):
             lowpan['nextHop'] = lowpan['route'][len(lowpan['route'])-1] #get next hop as this has to be the destination address, this is the last element on the list
 
             with self.bierLock:
-                if self.trackID == 1 or self.sendWithBier:
-                    if self.trackID ==4 or self.bierAuto:
-                        lowpan['bitmap'] = self._getBitmap(self.trackID, dst_addr)
-                    else:
-                        lowpan['bitmap'] = self.bierBitmap
+                if self.trackID ==4 or self.bierAuto:
+                    lowpan['bitmap'] = self._getBitmap(self.trackID, dst_addr)
+                elif self.trackID ==1 or self.sendWithBier:
+                    lowpan['bitmap'] = self.bierBitmap
 
             # turn dictionary of fields into raw bytes
             lowpan_bytes     = self.reassemble_lowpan(lowpan)
@@ -495,7 +494,7 @@ class OpenLbr(eventBusClient.eventBusClient):
         else :
             self.sendWithBier = False
             self.bierAuto     = False
-            if self.trackID == 3 :
+            if self.trackID == 4:
                 self.trackID = 1
             else :
                 self.trackID += 1
